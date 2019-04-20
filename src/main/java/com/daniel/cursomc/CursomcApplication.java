4af0c2adbfcaf0d8b666ed97f2353b60dc516f24
Daniel <danielfernando-2@outlook.com>
2019-04-20 13:54:11 -0300

    Pedido ,EstadoPagamento,Pagamento

--- src/main/java/com/daniel/cursomc/CursomcApplication.java
+++ src/main/java/com/daniel/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.daniel.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.daniel.cursomc.domain.Cidade;
 import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.domain.Endereco;
 import com.daniel.cursomc.domain.Estado;
+import com.daniel.cursomc.domain.Pagamento;
+import com.daniel.cursomc.domain.PagamentoComBoleto;
+import com.daniel.cursomc.domain.PagamentoComCartao;
+import com.daniel.cursomc.domain.Pedido;
 import com.daniel.cursomc.domain.Produto;
+import com.daniel.cursomc.domain.enums.EstadoPagamento;
 import com.daniel.cursomc.domain.enums.TipoCliente;
 import com.daniel.cursomc.repositories.CategoriaRepository;
 import com.daniel.cursomc.repositories.CidadeRepository;
 import com.daniel.cursomc.repositories.ClienteRepository;
 import com.daniel.cursomc.repositories.EnderecoRepository;
 import com.daniel.cursomc.repositories.EstadoRepository;
+import com.daniel.cursomc.repositories.PagamentoRepository;
+import com.daniel.cursomc.repositories.PedidoRepository;
 import com.daniel.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -29,18 +37,24 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -62,38 +76,49 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		Estado est1 = new Estado(null,"Minas Gerais");
-		Estado est2 = new Estado(null,"São paulo");
-		
-		Cidade c1 = new Cidade(null,"Uberlândia",est1);
-		Cidade c2 = new Cidade(null,"São paulo",est2);
-		Cidade c3 = new Cidade(null,"Campinas",est2);
-		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
 		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("36533243","984532143"));
-		
-		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 203","jardim","38220834",c1, cli1);
-		Endereco e2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","74898666",c2,cli1);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("36533243", "984532143"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "jardim", "38220834", c1, cli1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "74898666", c2, cli1);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		
+
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
-		
-		
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),null);
+		ped2.setPagamento(pagto2);
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
--- src/main/java/com/daniel/cursomc/domain/Cliente.java
+++ src/main/java/com/daniel/cursomc/domain/Cliente.java
@@ -32,6 +32,8 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
 
 	public Cliente() {
 
@@ -102,6 +104,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -126,5 +136,7 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+
 
 }
